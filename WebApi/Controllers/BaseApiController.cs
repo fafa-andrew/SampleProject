68c16cfaee7850c24b607e16b1d3e341d75a5a8f
Fafa Andrew Nyakpo <fafanyakpo@gmail.com>
2025-08-08 18:32:03 +0000

    Refactor BaseApiController for clarity, REST compliance, and testability

--- WebApi/Controllers/BaseApiController.cs
+++ WebApi/Controllers/BaseApiController.cs
@@ -1,24 +1,28 @@
-﻿using System.Net;
-using System.Net.Http;
+﻿// Cleanup notes:
+// 1. Method names like Found() were misleading, implying GET so I renamed for clarity
+// 2. Record creation endpoints now use CreatedAtRoute to return 201 and Location header
+// 3. I switched from HttpResponseMessage to IHttpActionResult for cleaner, testable code
+// 4. All helpers are protected to avoid appearing as public endpoints
+// 5. Removed redundant ControllerContext usage
+// 6. Switched methods to lambda versions for simplicity
+// 7. Renamed methods for clarity
+// 8. Added helper method for delete endpoints
+
+using System.Net;
 using System.Web.Http;
 
 namespace WebApi.Controllers
 {
     public abstract class BaseApiController : ApiController
     {
-        public HttpResponseMessage Found(object obj)
-        {
-            return ControllerContext.Request.CreateResponse(HttpStatusCode.OK, obj);
-        }
+        protected IHttpActionResult CreatedAt(object body, object routeValues, string routeName)
+        => CreatedAtRoute(routeName, routeValues, body);
 
-        public HttpResponseMessage Found()
-        {
-            return ControllerContext.Request.CreateResponse(HttpStatusCode.OK);
-        }
+        protected IHttpActionResult OkRequest(object body) => Ok(body);
 
-        public HttpResponseMessage DoesNotExist()
-        {
-            return ControllerContext.Request.CreateResponse(HttpStatusCode.NotFound);
-        }
+        protected IHttpActionResult RecordNotFound() => NotFound();
+
+        protected IHttpActionResult NoContentResponse() => StatusCode(HttpStatusCode.NoContent);
+
     }
 }
