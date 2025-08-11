8da5095679b3c4b6b69c23ac30da941c6a07692b
Fafa <fafanyakpo@gmail.com>
2025-08-11 06:55:47 -0700

    Added the product entity

---
+++ BusinessEntities/Product.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace BusinessEntities
+{
+    public class Product : IdObject
+    {
+        private string _name;
+        private string _description;
+        private decimal _price;
+        private int _stock;
+
+        public string Name => _name;
+
+        public string Description => _description; 
+
+        public decimal Price => _price;
+
+        public int Stock => _stock;
+
+        public void SetName(string name) => _name = name.Trim() ?? throw new ArgumentNullException(nameof(name));
+
+        public void SetDescription(string description) => _description = description.Trim() ?? string.Empty;
+
+        public void SetPrice(decimal price) 
+            => _price = price > 0 ? 
+            price : 
+            throw new ArgumentOutOfRangeException(nameof(price), "Price cannot be negative.");
+
+
+        public void SetStock(int stock)
+            => _stock = stock > 0 ?
+            stock :
+            throw new ArgumentOutOfRangeException(nameof(stock), "Stock cannot be negative.");
+
+        public void IncreaseStock(int qty)
+        {
+            if (qty <= 0) throw new ArgumentOutOfRangeException(nameof(qty));
+            _stock += qty;
+        }
+
+        public void DecreaseStock(int qty)
+        {
+            if (qty <= 0 || qty > _stock) throw new InvalidOperationException("Insufficient stock.");
+            _stock -= qty;
+        }
+
+        public bool IsInStock() => _stock > 0;
+
+    }
+}
